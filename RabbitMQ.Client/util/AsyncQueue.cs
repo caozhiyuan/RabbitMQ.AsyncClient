57ba05f5511d561074aec0f6edd6a4f42838b912
caozhiyuan <czy@qq.com>
2018-06-02 22:05:27 +0800

    init

--- RabbitMQ.Client/util/AsyncQueue.cs
+++ RabbitMQ.Client/util/AsyncQueue.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace RabbitMQ.Client.Impl
+namespace RabbitMQ.Util
 {
     public class AsyncQueue<T>
     {
