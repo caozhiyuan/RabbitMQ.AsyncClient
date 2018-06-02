13827a3f4b7fe7120011d900ec0eea698d728aa4
caozhiyuan <czy@qq.com>
2018-06-02 22:09:08 +0800

    init

--- RabbitMQ.Client.Test/Program.cs
+++ RabbitMQ.Client.Test/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQ.Client.Test
 {
@@ -35,6 +36,14 @@ namespace RabbitMQ.Client.Test
                 await channel.ExchangeDeclare("asynctest", "topic");
                 await channel.QueueDeclare("asynctest", true, false, false, null);
                 await channel.QueueBind("asynctest", "asynctest", "asynctest", null);
+
+                AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.Received += async (ch, ea) =>
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(ea.Body));
+                    await channel.BasicAck(ea.DeliveryTag, false);
+                };
+                var consumerTag = await channel.BasicConsume("asynctest", false, consumer);         
 
                 var messageBodyBytes = Encoding.UTF8.GetBytes(msg);
 
